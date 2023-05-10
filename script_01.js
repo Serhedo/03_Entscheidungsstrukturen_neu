6f3e1002cd8b64b7bc972a36d1d09196cbf058a5
Serhedo <serhat.alpayci@gmx.de>
2023-05-10 12:46:09 +0200

    03_Entscheidungsstrukturen

--- script_01.js
+++ script_01.js
@@ -101,17 +101,17 @@
 
 /********  Fallunterscheidung / SWITCH|CASE 2 *******/
 
-const cond = true;
-const a = 2;
+// const cond = true;
+// const a = 2;
 
-switch (cond) {
-    case (a==1):
-        console.log("a ist gleich 1!");
-        break;
-    case (a==2):
-        console.log("a ist gleich 2!");
-        break;
-    default:
-        console.log("a hat einen anderen Wert!");
-        break;
-}
+// switch (cond) {
+//     case (a==1):
+//         console.log("a ist gleich 1!");
+//         break;
+//     case (a==2):
+//         console.log("a ist gleich 2!");
+//         break;
+//     default:
+//         console.log("a hat einen anderen Wert!");
+//         break;
+// }
