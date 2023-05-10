e1cc0fdf1265f306657c923bdb93cc3daa5f0bed
Serhedo <serhat.alpayci@gmx.de>
2023-05-10 12:03:56 +0200

    03_Entscheidungsstrukturen_neu1

--- script_01.js
+++ script_01.js
@@ -72,14 +72,29 @@
 const firstName = "Jane";
 let job;
 
-job = "driver"; // .. fährt TAXI! / UBER
+job = "driver";     // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+job = "instructor"; // .. unterrichtet!
 
 switch (job) 
 {
     case "driver": // ( == )
         console.log (firstName + " fährt TAXI!");
         break;
-
+    case "diver":  // ( == )
+        console.log (firstName + " taucht im Rhein");
+        break;
+    case "artist":  // ( == )
+        console.log (firstName + " malt ein Bild!");
+        break;
+    case "teacher":  // ( == )
+    case "instructor":  // ( == )
+        console.log (firstName + " unterrichtet!");
+        break;
     default:
+        console.log(firstName + " macht etwas anderes!");
         break;
 }
