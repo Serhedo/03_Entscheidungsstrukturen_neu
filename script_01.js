96b1b25840ab778282285ae27f228ba7a55fc0f8
ShatterAr <sascha.s.89@gmx.de>
2023-05-10 11:28:23 +0200

    Ent

--- script_01.js
+++ script_01.js
@@ -3,3 +3,83 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
+// const ageJohn = 30;
+// const ageMark = 30;
+
+// // Deklaration
+// let isJohnOlder, isJohnEqual;
+
+// // Test | Logische Aussage 
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
+
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+//     console.log("John ist älter.")
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if(true)
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark) 
+// {
+//     console.log("John ist älter.");
+// } 
+// else 
+// {
+//     console.log("John ist jünger.")
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+
+// if(isJohnOlder) // 1. Test
+// {
+//     console.log("John ist älter.");
+// }
+// else if(isJohnEqual) // 1...n Alternative
+// {
+//     console.log("John ist gleich alt.");
+// }
+// // wenn alle vorherigen Tests fehlschlagen
+// else
+// {
+//     console.log("John ist jünger.");
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver"; // .. fährt TAXI! / UBER
+
+switch (job) 
+{
+    case "driver": // ( == )
+        console.log (firstName + " fährt TAXI!");
+        break;
+
+    default:
+        break;
+}
